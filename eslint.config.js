fc0f7d316897a6cd7abeb99607b8ba8c066618e6
ferdie bergado <ferdiebergado@gmail.com>
2026-02-04 09:00:03 +0000

    chore: Install and setup eslint-plugin-unicorn

--- eslint.config.js
+++ eslint.config.js
@@ -3,6 +3,7 @@ import pluginQuery from '@tanstack/eslint-plugin-query';
 import eslintConfigPrettier from 'eslint-config-prettier/flat';
 import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
+import eslintPluginUnicorn from 'eslint-plugin-unicorn';
 import { defineConfig, globalIgnores } from 'eslint/config';
 import globals from 'globals';
 import tseslint from 'typescript-eslint';
@@ -14,19 +15,33 @@ export default defineConfig([
     extends: [
       js.configs.recommended,
       tseslint.configs.recommended,
+
       reactHooks.configs.flat.recommended,
       reactRefresh.configs.vite,
       ...pluginQuery.configs['flat/recommended'],
+
+      eslintPluginUnicorn.configs.recommended,
+
       eslintConfigPrettier,
     ],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
     },
+    rules: {
+      'unicorn/prevent-abbreviations': 'off',
+      'unicorn/no-anonymous-default-export': 'off',
+      'unicorn/filename-case': 'off',
+    },
   },
   {
     files: ['netlify/**/*.{ts,js}', 'api/**/*.{ts,js}', 'shared/**/*.{ts,js}'],
-    extends: [js.configs.recommended, tseslint.configs.strictTypeChecked, eslintConfigPrettier],
+    extends: [
+      js.configs.recommended,
+      tseslint.configs.strictTypeChecked,
+      eslintPluginUnicorn.configs.recommended,
+      eslintConfigPrettier,
+    ],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.node,
@@ -40,6 +55,10 @@ export default defineConfig([
       '@typescript-eslint/await-thenable': 'error',
 
       '@typescript-eslint/consistent-type-definitions': 'off',
+
+      'unicorn/prevent-abbreviations': 'off',
+      'unicorn/no-anonymous-default-export': 'off',
+      'unicorn/filename-case': 'off',
     },
   },
 ]);
