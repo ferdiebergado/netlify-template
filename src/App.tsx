704982027723d422ec140ce3ba29fdc48b833ba3
ferdie bergado <ferdiebergado@gmail.com>
2026-02-07 11:36:36 +0800

    refactor: Move providers to App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,28 @@
+import { ErrorFallback } from '@/components/ErrorFallback';
+import { QueryClient, QueryClientProvider, QueryErrorResetBoundary } from '@tanstack/react-query';
+import { ErrorBoundary } from 'react-error-boundary';
 import { RouterRoot } from 'waymark';
 import { routes } from './routes';
 
+const queryClient = new QueryClient();
+
 export function App() {
-  return <RouterRoot routes={routes} />;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <QueryErrorResetBoundary>
+        {({ reset }) => (
+          <ErrorBoundary
+            fallbackRender={({ error, resetErrorBoundary }) => (
+              <ErrorFallback error={error} resetErrorBoundary={resetErrorBoundary} />
+            )}
+            onReset={reset}
+          >
+            <RouterRoot routes={routes} />;
+          </ErrorBoundary>
+        )}
+      </QueryErrorResetBoundary>
+    </QueryClientProvider>
+  );
 }
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,28 +1,10 @@
-import { ErrorFallback } from '@/components/ErrorFallback';
-import { QueryClient, QueryClientProvider, QueryErrorResetBoundary } from '@tanstack/react-query';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { ErrorBoundary } from 'react-error-boundary';
 import App from './App';
 import './index.css';
 
-const queryClient = new QueryClient();
-
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <QueryErrorResetBoundary>
-        {({ reset }) => (
-          <ErrorBoundary
-            fallbackRender={({ error, resetErrorBoundary }) => (
-              <ErrorFallback error={error} resetErrorBoundary={resetErrorBoundary} />
-            )}
-            onReset={reset}
-          >
-            <App />
-          </ErrorBoundary>
-        )}
-      </QueryErrorResetBoundary>
-    </QueryClientProvider>
+    <App />
   </StrictMode>
 );
