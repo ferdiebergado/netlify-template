8d0f923880cc1584a929b9d0e727ddd0d5df1f4d
ferdie bergado <ferdiebergado@gmail.com>
2026-02-08 18:50:00 +0800

    refactor: Change background color to muted

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,15 +1,15 @@
-import { ModeToggle } from '@/features/dark-mode/components/mode-toggle';
 import { Outlet } from 'react-router';
+import { ModeToggle } from '../features/dark-mode/components/mode-toggle';
 
 export default function Layout() {
   return (
-    <>
+    <div className="bg-muted min-h-screen">
       <header className="p-5">
         <ModeToggle />
       </header>
       <main className="flex justify-center p-8">
         <Outlet />
       </main>
-    </>
+    </div>
   );
 }
