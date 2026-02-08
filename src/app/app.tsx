2eb7f6c9d6fece2425e4dffb5bbff6283423709b
ferdie bergado <ferdiebergado@gmail.com>
2026-02-08 10:06:04 +0800

    refactor: Move errror boundary to App component

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,10 +1,24 @@
+import { QueryErrorResetBoundary } from '@tanstack/react-query';
+import { ErrorBoundary } from 'react-error-boundary';
+import FallbackPage from './pages/fallback-page';
 import Provider from './provider';
 import Router from './router';
 
 export function App() {
   return (
     <Provider>
-      <Router />
+      <QueryErrorResetBoundary>
+        {({ reset }) => (
+          <ErrorBoundary
+            fallbackRender={({ error, resetErrorBoundary }) => (
+              <FallbackPage error={error} resetErrorBoundary={resetErrorBoundary} />
+            )}
+            onReset={reset}
+          >
+            <Router />
+          </ErrorBoundary>
+        )}
+      </QueryErrorResetBoundary>
     </Provider>
   );
 }
--- src/app/provider.tsx
+++ src/app/provider.tsx
@@ -1,8 +1,7 @@
-import { QueryClient, QueryClientProvider, QueryErrorResetBoundary } from '@tanstack/react-query';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import type { ReactNode } from 'react';
-import { ErrorBoundary } from 'react-error-boundary';
 import { BrowserRouter } from 'react-router';
-import FallbackPage from './pages/fallback-page';
+
 const queryClient = new QueryClient();
 
 type ProviderProps = {
@@ -12,20 +11,7 @@ type ProviderProps = {
 export default function Provider({ children }: ProviderProps) {
   return (
     <QueryClientProvider client={queryClient}>
-      <BrowserRouter>
-        <QueryErrorResetBoundary>
-          {({ reset }) => (
-            <ErrorBoundary
-              fallbackRender={({ error, resetErrorBoundary }) => (
-                <FallbackPage error={error} resetErrorBoundary={resetErrorBoundary} />
-              )}
-              onReset={reset}
-            >
-              {children}
-            </ErrorBoundary>
-          )}
-        </QueryErrorResetBoundary>
-      </BrowserRouter>
+      <BrowserRouter>{children}</BrowserRouter>
     </QueryClientProvider>
   );
 }
