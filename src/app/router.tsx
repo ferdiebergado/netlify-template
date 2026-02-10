4cdf98889bcf9df766e2928a1b4a46a7b00937d0
ferdie bergado <ferdiebergado@gmail.com>
2026-02-10 18:03:48 +0800

    refactor: Lazy load NotFoundPage

--- src/app/router.tsx
+++ src/app/router.tsx
@@ -3,10 +3,10 @@ import PublicLayout from '@/components/public-layout';
 import { lazy } from 'react';
 import { useRoutes, type RouteObject } from 'react-router';
 import AuthGuard from '../features/auth/components/auth-guard';
-import NotFoundPage from './pages/not-found-page';
 
 const LoginPage = lazy(() => import('../features/auth/components/login-page'));
 const Home = lazy(() => import('./pages/home'));
+const NotFoundPage = lazy(() => import('./pages/not-found-page'));
 
 const routes: RouteObject[] = [
   {
