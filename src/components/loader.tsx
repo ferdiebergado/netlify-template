c089410558b5b2c9241749c5f0a06085db365920
ferdie bergado <ferdiebergado@gmail.com>
2026-02-10 16:00:30 +0800

    refactor: Set background to muted

--- src/components/loader.tsx
+++ src/components/loader.tsx
@@ -2,7 +2,7 @@ import { LoaderIcon } from 'lucide-react';
 
 export default function Loader() {
   return (
-    <div className="flex h-screen items-center justify-center">
+    <div className="bg-muted flex h-screen items-center justify-center">
       <LoaderIcon className="mx-3 animate-spin" />
       <span>Please wait...</span>
     </div>
