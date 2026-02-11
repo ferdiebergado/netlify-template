541fdfc1ad58bf091191bca761c8cbf9cbe7aaf4
ferdie bergado <ferdiebergado@gmail.com>
2026-02-11 05:51:01 +0000

    refactor: Add app sidebar

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,5 +1,9 @@
 import { Outlet } from 'react-router';
+
 import { ModeToggle } from '../features/dark-mode/components/mode-toggle';
+import { AppSidebar } from './app-sidebar';
+import { SidebarProvider, SidebarTrigger } from './ui/sidebar';
+import { TooltipProvider } from './ui/tooltip';
 
 export default function Layout() {
   return (
@@ -7,9 +11,15 @@ export default function Layout() {
       <header className="p-5">
         <ModeToggle />
       </header>
-      <main className="flex justify-center p-8">
-        <Outlet />
-      </main>
+      <TooltipProvider>
+        <SidebarProvider>
+          <AppSidebar />
+          <main className="flex justify-center p-8">
+            <SidebarTrigger />
+            <Outlet />
+          </main>
+        </SidebarProvider>
+      </TooltipProvider>
     </div>
   );
 }
