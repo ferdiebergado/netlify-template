8b2017da223f256342723ad204838435352e69e0
ferdie bergado <ferdiebergado@gmail.com>
2026-02-14 11:08:34 +0800

    refactor: Log actions

--- src/features/auth/api.ts
+++ src/features/auth/api.ts
@@ -4,9 +4,11 @@ import { paths } from '../../app/routes';
 import { api } from '../../lib/client';
 
 export async function login(token: string) {
+  console.log('logging in...');
   return await api.post<LoginData>(paths.login, { token });
 }
 
 export async function fetchMe() {
+  console.log('fetching current user...');
   return await api.get<User>('/me');
 }
