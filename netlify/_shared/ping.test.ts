b3a603467e0c7f3021a26c07283e83317cf576c1
ferdie bergado <ferdiebergado@gmail.com>
2026-02-14 21:44:17 +0800

    refactor: Remove ping endpoint

--- netlify/_shared/ping.test.ts
+++
@@ -1,9 +0,0 @@
-import { describe, expect, it } from 'vitest';
-import { db } from './db';
-import { checkHealth } from './ping';
-
-describe('checkHealth', () => {
-  it('should resolve to undefined', async () => {
-    await expect(checkHealth(db)).resolves.toBe(undefined);
-  });
-});
--- netlify/_shared/ping.ts
+++
@@ -1,5 +0,0 @@
-import { type Database } from './db';
-
-export async function checkHealth(db: Database) {
-  await db.execute('SELECT 1');
-}
