3373d6a65819061a693c9e1fcbca8341f1823e21
ferdie bergado <ferdiebergado@gmail.com>
2026-02-16 18:11:32 +0800

    refactor: Specify height of parent div

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -6,7 +6,7 @@ import { SidebarTrigger } from './ui/sidebar';
 export default function Header() {
   return (
     <header className="flex h-16 shrink-0 items-center gap-2">
-      <div className="flex items-center gap-2 px-4">
+      <div className="flex h-4 items-center gap-2 px-4">
         <SidebarTrigger className="-ml-1" />
         <Separator orientation="vertical" className="mr-2 data-[orientation=vertical]:h-4" />
         <Breadcrumbs />
