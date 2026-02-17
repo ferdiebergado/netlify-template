7fb2a3f4809798f34923ead436906899a36cc864
ferdie bergado <ferdiebergado@gmail.com>
2026-02-17 09:09:25 +0800

    chore: Install and setup react query devtools

--- src/app/provider.tsx
+++ src/app/provider.tsx
@@ -1,13 +1,14 @@
 import { Toaster } from '@/components/ui/sonner';
 import { GoogleOAuthProvider } from '@react-oauth/google';
 import { QueryCache, QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import type { ReactNode } from 'react';
 import { BrowserRouter } from 'react-router';
 import { toast } from 'sonner';
 
 import DarkModeProvider from '../components/dark-mode/mode-provider';
 import { env } from '../config';
-import CurrentUserProvider from '../features/auth/components/current-user-provider';
+import UserProvider from '../features/auth/components/current-user-provider';
 
 const queryClient = new QueryClient({
   queryCache: new QueryCache({
@@ -26,12 +27,13 @@ export default function Provider({ children }: ProviderProps) {
     <QueryClientProvider client={queryClient}>
       <BrowserRouter>
         <GoogleOAuthProvider clientId={env.VITE_GOOGLE_CLIENT_ID} locale="en-US">
-          <CurrentUserProvider>
+          <UserProvider>
             <DarkModeProvider>
               {children}
               <Toaster position="top-right" richColors />
+              <ReactQueryDevtools initialIsOpen={false} />
             </DarkModeProvider>
-          </CurrentUserProvider>
+          </UserProvider>
         </GoogleOAuthProvider>
       </BrowserRouter>
     </QueryClientProvider>
