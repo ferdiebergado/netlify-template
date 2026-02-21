0952724e182d083f9e718cb7a6bd04a10305720b
ferdie bergado <ferdiebergado@gmail.com>
2026-02-21 17:11:00 +0800

    refactor: Redirect back if possible when logged in user navigates to login page

--- src/features/auth/components/require-guest.tsx
+++ src/features/auth/components/require-guest.tsx
@@ -1,11 +1,31 @@
-import { Navigate, Outlet } from 'react-router';
+import { useCallback, useEffect } from 'react';
+import { Outlet, useLocation, useNavigate, type To } from 'react-router';
 
 import { useCurrentUser } from '../hooks';
 
 export default function RequireGuest() {
   const { isAuthenticated } = useCurrentUser();
+  const { state } = useLocation();
+  const navigate = useNavigate();
 
-  if (isAuthenticated) return <Navigate to="/" replace />;
+  const redirect = useCallback((to: To) => navigate(to, { replace: true }), [navigate]);
+
+  useEffect(() => {
+    if (isAuthenticated) {
+      const intendedDestination = state?.from;
+
+      if (intendedDestination) {
+        redirect(intendedDestination);
+      } else if (globalThis.history.length > 2) {
+        redirect(-1);
+      } else {
+        redirect('/');
+      }
+    }
+  }, [isAuthenticated, redirect, state]);
+
+  // eslint-disable-next-line unicorn/no-null
+  if (isAuthenticated) return null;
 
   return <Outlet />;
 }
