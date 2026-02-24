72160907472ef70a409a0d5a28b67808d33c80f2
ferdie bergado <ferdiebergado@gmail.com>
2026-02-24 08:45:34 +0800

    refactor: Use declarative redirect

--- src/features/auth/components/require-guest.tsx
+++ src/features/auth/components/require-guest.tsx
@@ -1,31 +1,16 @@
-import { useCallback, useEffect } from 'react';
-import { Outlet, useLocation, useNavigate, type To } from 'react-router';
+import { Navigate, Outlet, useLocation } from 'react-router';
 
 import { useCurrentUser } from '../hooks';
 
 export default function RequireGuest() {
   const { isAuthenticated } = useCurrentUser();
   const { state } = useLocation();
-  const navigate = useNavigate();
 
-  const redirect = useCallback((to: To) => navigate(to, { replace: true }), [navigate]);
-
-  useEffect(() => {
-    if (isAuthenticated) {
-      const intendedDestination = state?.from;
-
-      if (intendedDestination) {
-        redirect(intendedDestination);
-      } else if (globalThis.history.length > 2) {
-        redirect(-1 as never);
-      } else {
-        redirect('/');
-      }
-    }
-  }, [isAuthenticated, redirect, state]);
-
-  // eslint-disable-next-line unicorn/no-null
-  if (isAuthenticated) return null;
+  if (isAuthenticated) {
+    const to = state?.from;
+    if (to) return <Navigate to={to} replace />;
+    return <Navigate to="/" replace />;
+  }
 
   return <Outlet />;
 }
