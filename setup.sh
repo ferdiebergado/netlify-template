f9c962581c2d2e344a0c1c80bf61b87be7a2f3ef
ferdie bergado <ferdiebergado@gmail.com>
2026-02-28 19:59:16 +0800

    chore: Add create-db

--- setup.sh
+++ setup.sh
@@ -3,4 +3,5 @@
 [ -z $(command -v netlify) ] && npm i -g netlify-cli &
 [ -z "$(npx playwright install --list | grep -i ".cache/ms-playwright/chromium")" ] && npx playwright install --with-deps &
 [ ! -f "./public/mockServiceWorker.js" ] && npx msw init ./public --save &
+[ ! -f "./local.db" ] && npm run create-db &
 [ ! -f .env ] && mv .env.example .env
