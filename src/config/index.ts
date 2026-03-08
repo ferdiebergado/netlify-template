ef64fea636168122c5dbbc6f2e82bf018b189088
ferdie bergado <ferdiebergado@gmail.com>
2026-03-08 11:53:09 +0800

    refactor: Add validation error message

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,7 +1,7 @@
 import * as z from 'zod';
 
 const EnvSchema = z.object({
-  VITE_CLERK_PUBLISHABLE_KEY: z.string(),
+  VITE_CLERK_PUBLISHABLE_KEY: z.string().min(1, 'VITE_CLERK_PUBLISHABLE_KEY is not set.'),
 });
 
 export const env = EnvSchema.parse(import.meta.env);
