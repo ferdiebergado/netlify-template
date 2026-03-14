08fc1fdd188b69b8979bae71423935f49c6dfca2
ferdie bergado <ferdiebergado@gmail.com>
2026-03-14 14:50:58 +0800

    refactor: Remove method from config

--- netlify/edge-functions/csrf.ts
+++ netlify/edge-functions/csrf.ts
@@ -3,28 +3,31 @@ import type { Failure } from '@shared/types/api';
 
 export const config: Config = {
   path: '/.netlify/functions/*',
-  method: ['POST', 'PUT', 'DELETE'],
+  method: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
 };
 
 export default (req: Request) => {
-  console.log('Checking fetch metadata headers...');
+  console.log('Checking fetch metadata...');
 
   if (
-    req.headers.get('Sec-Fetch-Mode') === 'navigate' &&
     req.method === 'GET' &&
+    req.headers.get('Sec-Fetch-Mode') === 'navigate' &&
     req.headers.get('Sec-Fetch-Dest') !== 'object' &&
     req.headers.get('Sec-Fetch-Dest') !== 'embed'
-  ) {
+  )
     return;
-  }
 
-  const fetchSite = req.headers.get('Sec-Fetch-Site');
+  const unsafeMethods = ['POST', 'PUT', 'PATCH', 'DELETE'];
 
-  if (fetchSite !== 'same-origin') {
-    const payload: Failure = {
-      status: 'failed',
-      error: 'cross-origin requests are not allowed',
-    };
-    return Response.json(payload, { status: 401 });
+  if (unsafeMethods.includes(req.method)) {
+    const fetchSite = req.headers.get('Sec-Fetch-Site');
+
+    if (fetchSite !== 'same-origin') {
+      const payload: Failure = {
+        status: 'failed',
+        error: 'cross-origin requests are not allowed',
+      };
+      return Response.json(payload, { status: 401 });
+    }
   }
 };
