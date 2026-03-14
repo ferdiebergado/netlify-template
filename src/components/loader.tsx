6206add73c4c5628d694fd38a11c6079bb1b5d36
ferdie bergado <ferdiebergado@gmail.com>
2026-03-14 10:36:16 +0800

    refactor: Change div to p tag

--- src/components/loader.tsx
+++ src/components/loader.tsx
@@ -9,9 +9,9 @@ type LoaderProps = ComponentProps<'div'> & {
 
 export default function Loader({ className, text = 'Please wait...', ...props }: LoaderProps) {
   return (
-    <div className={cn('space-x-3', className)} {...props}>
+    <p className={cn('space-x-3', className)} {...props}>
       <LoaderIcon className="animate-spin" />
       <span>{text}</span>
-    </div>
+    </p>
   );
 }
