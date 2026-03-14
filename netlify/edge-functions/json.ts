13873bb2eca8ef94d5dd10139e4e0692a1644391
ferdie bergado <ferdiebergado@gmail.com>
2026-03-14 14:51:34 +0800

    feat: Add content-type middleware

---
+++ netlify/edge-functions/json.ts
@@ -0,0 +1,21 @@
+import type { Config } from '@netlify/edge-functions';
+import type { Failure } from '@shared/types/api';
+
+export const config: Config = {
+  path: '/.netlify/functions/*',
+  method: ['POST', 'PUT', 'PATCH'],
+};
+
+export default (req: Request) => {
+  console.log('Validating content-type...');
+
+  const contentType = req.headers.get('Content-Type');
+
+  if (!contentType || !/^application\/json(;.*)?$/i.test(contentType)) {
+    const payload: Failure = {
+      status: 'failed',
+      error: 'Unsupported data type',
+    };
+    return Response.json(payload, { status: 415 });
+  }
+};
