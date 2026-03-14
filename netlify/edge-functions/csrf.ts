7d5d6576b079181bf687403dcf83781ba99af494
ferdie bergado <ferdiebergado@gmail.com>
2026-03-14 20:59:33 +0800

    refactor: Do not support top level navigation

--- netlify/edge-functions/csrf.ts
+++ netlify/edge-functions/csrf.ts
@@ -3,31 +3,19 @@ import type { Failure } from '@shared/types/api';
 
 export const config: Config = {
   path: '/.netlify/functions/*',
-  method: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
+  method: ['POST', 'PUT', 'PATCH', 'DELETE'],
 };
 
 export default (req: Request) => {
   console.log('Checking fetch metadata...');
 
-  if (
-    req.method === 'GET' &&
-    req.headers.get('Sec-Fetch-Mode') === 'navigate' &&
-    req.headers.get('Sec-Fetch-Dest') !== 'object' &&
-    req.headers.get('Sec-Fetch-Dest') !== 'embed'
-  )
-    return;
+  const fetchSite = req.headers.get('Sec-Fetch-Site');
 
-  const unsafeMethods = ['POST', 'PUT', 'PATCH', 'DELETE'];
-
-  if (unsafeMethods.includes(req.method)) {
-    const fetchSite = req.headers.get('Sec-Fetch-Site');
-
-    if (!fetchSite || fetchSite !== 'same-origin') {
-      const payload: Failure = {
-        status: 'failed',
-        error: 'cross-origin requests are not allowed',
-      };
-      return Response.json(payload, { status: 401 });
-    }
+  if (!fetchSite || fetchSite !== 'same-origin') {
+    const payload: Failure = {
+      status: 'failed',
+      error: 'cross-origin requests are not allowed',
+    };
+    return Response.json(payload, { status: 401 });
   }
 };
