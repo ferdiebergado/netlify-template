e2c7111615c4bb12d3602a481f3b7c4b64ebc3b1
ferdie bergado <ferdiebergado@gmail.com>
2026-03-15 05:53:46 +0000

    refactor: Extract migration file into a constant

--- api/constants.ts
+++ api/constants.ts
@@ -8,3 +8,5 @@ export const ALLOWED_ORIGINS = [
 export const SESSION_COOKIE_NAME = '__Secure-session';
 export const SESSIONID_LENGTH = 32;
 export const SESSION_DURATION_MINUTES = 60 * 24 * 30 * 3;
+
+export const MIGRATION_FILE = 'init.sql';
