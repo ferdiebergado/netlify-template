4f612f7d1fb00453f225f8d7506c205e1896e618
ferdie bergado <ferdiebergado@gmail.com>
2026-03-15 04:42:06 +0000

    chore: Run npm install first

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env sh
-
+[ ! -d "./node_modules" ] && npm install
 [ -z $(command -v netlify) ] && npm i -g netlify-cli &
 [ -z "$(npx playwright install --list | grep -i ".cache/ms-playwright/chromium")" ] && npx playwright install --with-deps &
 [ ! -f "./public/mockServiceWorker.js" ] && npx msw init ./public --save &
