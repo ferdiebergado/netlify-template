07fe60224c68e0320ed6689a06c2cf88949ed4bc
ferdie bergado <ferdiebergado@gmail.com>
2026-03-15 03:10:06 +0000

    chore: Copy .env.example instead of renaming it

--- setup.sh
+++ setup.sh
@@ -4,4 +4,4 @@
 [ -z "$(npx playwright install --list | grep -i ".cache/ms-playwright/chromium")" ] && npx playwright install --with-deps &
 [ ! -f "./public/mockServiceWorker.js" ] && npx msw init ./public --save &
 [ ! -f "./local.db" ] && npm run create-db &
-[ ! -f .env ] && mv .env.example .env
+[ ! -f .env ] && cp .env.example .env
