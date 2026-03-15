b7d48c91e21e28d97f7916415c95c5ccdf347827
ferdie bergado <ferdiebergado@gmail.com>
2026-03-15 09:40:39 +0800

    refactor: Set default padding to 0

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -6,7 +6,7 @@ import UserMenu from './user-menu';
 
 export default function Header() {
   return (
-    <header className="flex h-16 w-full items-center gap-2 sm:p-6">
+    <header className="flex h-16 w-full items-center gap-2 p-0 sm:p-6">
       <div className="flex h-4 items-center gap-2 px-4">
         <SidebarTrigger className="-ml-1" />
         <Separator orientation="vertical" className="mr-2 data-[orientation=vertical]:h-4" />
