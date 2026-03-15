1cc094b8e35d562acfea2a76b9a2218bd9d80602
ferdie bergado <ferdiebergado@gmail.com>
2026-03-15 06:10:03 +0000

    test: Add findSession unit test

--- api/session.repo.test.ts
+++ api/session.repo.test.ts
@@ -2,40 +2,55 @@ import { beforeEach, describe, expect, it } from 'vitest';
 
 import type { User } from '@shared/schemas/user.schema';
 import { createTestDB } from '@testing/node/db';
+import { afterEach } from 'node:test';
 import type { Database } from './db';
 import type { Session } from './session';
 import { createSession, findSession } from './session.repo';
 import { upsertUser } from './user.repo';
 
 describe('session repo', () => {
+  const mockUser: User = {
+    googleId: 'abc',
+    name: 'antonio',
+    email: 'tatayoyo@gmail.com',
+  };
+
+  const mockSession: Session = {
+    sessionId: '123',
+    userId: mockUser.googleId,
+    userAgent: 'vitest',
+    ip: '127.0.0.1',
+    expiresAt: new Date(),
+  };
+
   let db: Database;
 
   beforeEach(async () => {
     db = await createTestDB();
+    await upsertUser(db, mockUser);
+  });
+
+  afterEach(() => {
+    db.close();
   });
 
   describe('createSession', () => {
     it('should save the session', async () => {
-      const user: User = {
-        googleId: 'abc',
-        name: 'antonio',
-        email: 'tatayoyo@gmail.com',
-      };
-      await upsertUser(db, user);
-
-      const mockSession: Session = {
-        sessionId: 'abc',
-        userId: user.googleId,
-        userAgent: 'vitest',
-        ip: '127.0.0.1',
-        expiresAt: new Date(),
-      };
-
       await createSession(db, mockSession);
 
       const session = await findSession(db, mockSession.sessionId);
 
       expect(session).toStrictEqual(mockSession);
+    });
+  });
+
+  describe('findSession', () => {
+    it('should find the session', async () => {
+      await createSession(db, mockSession);
+
+      const foundSession = await findSession(db, mockSession.sessionId);
+
+      expect(foundSession).toStrictEqual(mockSession);
     });
   });
 });
