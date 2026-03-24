c523e1c45a7cefe80386ed765428c008370ea939
ferdie bergado <ferdiebergado@gmail.com>
2026-03-24 21:41:29 +0800

    refactor: Use iso 8601 date string as default timestamp

--- init.sql
+++ init.sql
@@ -5,8 +5,8 @@ CREATE TABLE IF NOT EXISTS users (
     email TEXT,
     picture TEXT,
     last_login_at TEXT,
-    created_at TEXT DEFAULT CURRENT_TIMESTAMP,
-    updated_at TEXT DEFAULT CURRENT_TIMESTAMP,
+    created_at TEXT DEFAULT (STRFTIME ('%Y-%m-%dT%H:%M:%fZ', 'NOW')),
+    updated_at TEXT DEFAULT (STRFTIME ('%Y-%m-%dT%H:%M:%fZ', 'NOW')),
     deleted_at TEXT
 );
 
@@ -24,9 +24,9 @@ CREATE TABLE IF NOT EXISTS sessions (
     city TEXT,
     country TEXT,
     expires_at TEXT NOT NULL,
-    last_active_at TEXT DEFAULT CURRENT_TIMESTAMP,
-    created_at TEXT DEFAULT CURRENT_TIMESTAMP,
-    updated_at TEXT DEFAULT CURRENT_TIMESTAMP,
+    last_active_at TEXT DEFAULT (STRFTIME ('%Y-%m-%dT%H:%M:%fZ', 'NOW')),
+    created_at TEXT DEFAULT (STRFTIME ('%Y-%m-%dT%H:%M:%fZ', 'NOW')),
+    updated_at TEXT DEFAULT (STRFTIME ('%Y-%m-%dT%H:%M:%fZ', 'NOW')),
     deleted_at TEXT,
     is_revoked INTEGER DEFAULT 0 CHECK (is_revoked IN (0, 1)),
     FOREIGN KEY (user_id) REFERENCES users (user_id)
