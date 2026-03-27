133d96da56ede9fc912692b5ec3a5c690c8f4673
ferdie bergado <ferdiebergado@gmail.com>
2026-03-27 15:06:22 +0800

    refactor: Disable zod jit

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,12 +1,21 @@
 import * as z from 'zod';
 
+z.config({
+  jitless: true,
+});
+
 const envSchema = z.object({
   VITE_APP_TITLE: z.string().min(1),
   VITE_GOOGLE_CLIENT_ID: z.string({ error: 'VITE_GOOGLE_CLIENT_ID is not set.' }).min(1),
-  VITE_APP_HOST: z.url({ error: 'VITE_APP_HOST is not set.' }),
+  VITE_APP_HOST: z.url({ error: 'VITE_APP_HOST must be a valid URL' }),
 });
 
 const { success, error, data } = envSchema.safeParse(import.meta.env);
-if (!success) throw new Error(z.prettifyError(error));
+if (!success) {
+  const errors = z.flattenError(error).fieldErrors;
+  console.error('Environment variable validation failed:', errors);
+
+  throw new Error('Invalid environment variables');
+}
 
 export const env = data;
