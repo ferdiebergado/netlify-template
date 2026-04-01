a906a777c460b830e78a4f60b7faeb426c5c2c2e
ferdie bergado <ferdiebergado@gmail.com>
2026-04-01 15:47:30 +0800

    chore: Do not install netlify-cli

--- setup.sh
+++ setup.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env sh
 [ ! -d "./node_modules" ] && npm install
-[ -z $(command -v netlify) ] && npm i -g netlify-cli &
 [ -z "$(npx playwright install --list | grep -i ".cache/ms-playwright/chromium")" ] && npx playwright install --with-deps &
 [ ! -f "./public/mockServiceWorker.js" ] && npx msw init ./public --save &
 [ ! -f "./local.db" ] && npm run create-db &
