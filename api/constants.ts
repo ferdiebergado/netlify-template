f1b3017065711801e51ee591fff09e64634489ca
ferdie bergado <ferdiebergado@gmail.com>
2026-04-02 17:28:46 +0800

    refactor: Remove unused constants

--- api/constants.ts
+++ api/constants.ts
@@ -1,10 +1,3 @@
-export const ALLOWED_ORIGINS = [
-  'http://localhost:8888',
-  'http://localhost:5173',
-  'http://127.0.0.1:5173',
-  'https://tsferdie.netlify.app',
-];
-
 export const SESSION_COOKIE_NAME = '__Secure-session';
 export const SESSIONID_LENGTH = 32;
 export const SESSION_DURATION_MINUTES = 60 * 24 * 30 * 3;
