8bccd0010c13eb7ca1a47b31149cb0af0ac32cec
ferdie bergado <ferdiebergado@gmail.com>
2026-04-02 11:05:42 +0800

    refactor: Use existing constant for issuer

--- api/auth.ts
+++ api/auth.ts
@@ -1,10 +1,10 @@
 import { OAuth2Client } from 'google-auth-library';
 
+import { GOOGLE_ACCOUNTS_ORIGIN } from '@shared/constants';
 import type { User } from '@shared/schemas/user.schema';
 import { env } from './config';
 import { UnauthorizedError } from './errors';
 
-const ISSUER = 'https://accounts.google.com';
 const { GOOGLE_CLIENT_ID } = env;
 const client = new OAuth2Client(GOOGLE_CLIENT_ID);
 
@@ -18,7 +18,7 @@ export async function verifyToken(token: string): Promise<User> {
 
   if (tokenPayload === undefined) throw new UnauthorizedError('Invalid token payload');
 
-  if (tokenPayload.iss !== ISSUER) throw new UnauthorizedError('Invalid issuer');
+  if (tokenPayload.iss !== GOOGLE_ACCOUNTS_ORIGIN) throw new UnauthorizedError('Invalid issuer');
 
   return {
     googleId: tokenPayload.sub,
