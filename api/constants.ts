da4505bfd65d9d70bc5000151e6e600b674a94cb
ferdie bergado <ferdiebergado@gmail.com>
2026-04-04 16:13:59 +0800

    refactor: Replace session prefix with __Host

--- api/constants.ts
+++ api/constants.ts
@@ -1,6 +1,6 @@
-export const SESSION_COOKIE_NAME = '__Secure-session';
+export const SESSION_COOKIE_NAME = '__Host-session';
 export const SESSIONID_LENGTH = 32;
-export const SESSION_DURATION_MINUTES = 60 * 24 * 30 * 3;
+export const SESSION_DURATION_MINUTES = 60 * 24 * 30 * 3; // 90 days
 export const SESSION_HEADER_NAME = 'X-Session-Id';
 
 export const MIGRATION_FILE = 'init.sql';
