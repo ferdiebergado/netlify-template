4aad38a407cc4b6dbe27baeaab1d18e36ef27c26
ferdie bergado <ferdiebergado@gmail.com>
2026-04-24 11:02:19 +0800

    refactor: Reorder constant declarations

--- api/constants.ts
+++ api/constants.ts
@@ -1,8 +1,8 @@
+export const MIGRATION_FILE = 'init.sql';
+
 export const SESSION = {
   COOKIE_NAME: '__Host-session',
   ID_LENGTH: 32,
   DURATION_MINUTES: 60 * 24 * 30 * 3, // 90 days
   HEADER_NAME: 'x-session-id',
 } as const;
-
-export const MIGRATION_FILE = 'init.sql';
